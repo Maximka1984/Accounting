32356a5b12a162c8c6a4cdb387014d3e1de41bd3
Max <alkansobaka@email.com>
2024-11-25 12:39:01 +0300

    initial commit

---
+++ application/db/people.py
@@ -0,0 +1,2 @@
+def get_employees(self):
+    print(__name__)
---
+++ main.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    from application import salary
+    from application.db import people
+    import datetime
